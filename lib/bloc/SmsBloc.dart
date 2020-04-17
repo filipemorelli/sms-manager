9d32ace68b349ba5aad886e0cff3e76f0d327375
Filipe Morelli <filipe.souza@monetizze.com.br>
2020-04-17 20:13:32 -0300

    Update messages in main screen

--- lib/bloc/SmsBloc.dart
+++ lib/bloc/SmsBloc.dart
@@ -12,17 +12,33 @@ class SmsBloc {
 
   factory SmsBloc() => instance;
 
+  SmsReceiver _smsReceiver;
+  SmsQuery _smsQuery;
+
   SmsBloc._() {
     _streamController = StreamController.broadcast();
+    _smsQuery = SmsQuery();
+    _smsReceiver = SmsReceiver();
   }
 
   loadSmsthreads() async {
     try {
-      var smsQuery = SmsQuery();
-      _streamController.sink.add(await smsQuery.getAllThreads);
+      _streamController.sink.add(await _smsQuery.getAllThreads);
+      await _updateMessages();
     } catch (e, stackTrace) {
       log(e.toString(), name: "SmsBloc.loadSmsthreads", stackTrace: stackTrace);
     }
+  }
+
+  _updateMessages() async {
+    _smsReceiver.onSmsReceived.listen((smsReceiver) async {
+      try {
+        _streamController.sink.add(await _smsQuery.getAllThreads);
+      } catch (e, stackTrace) {
+        log(e.toString(),
+            name: "SmsBloc._updateMessages", stackTrace: stackTrace);
+      }
+    });
   }
 
   dispose() {
