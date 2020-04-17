d0df702dc99d99b4a1aaedacdfda99803b77ab5e
Filipe Morelli <filipe.souza@monetizze.com.br>
2020-04-17 20:38:20 -0300

    Create not found screen

--- lib/pages/404/not_found.dart
+++ lib/pages/404/not_found.dart
@@ -8,6 +8,28 @@ class NotFoundScreen extends StatefulWidget {
 class _NotFoundScreenState extends State<NotFoundScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+          color: Colors.black,
+        ),
+        centerTitle: true,
+        title: Text(
+          "Not Found",
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: Text("Screen not built yet."),
+      ),
+    );
   }
 }
