38df796d1b20a2ff1ec01cb0165b89573c4c2da6
Filipe Morelli <filipe.souza@monetizze.com.br>
2020-04-17 20:52:24 -0300

    Add two back button press Android

--- lib/pages/sms/sms_screen.dart
+++ lib/pages/sms/sms_screen.dart
@@ -7,6 +7,7 @@ import 'package:smsmanager/widgets/FloatActionButton.dart';
 import 'package:smsmanager/widgets/SmsPopupMenuButton.dart';
 
 import '../../bloc/SmsBloc.dart';
+import '../../globals/functions.dart';
 
 class SmsScreen extends StatefulWidget {
   @override
@@ -17,6 +18,8 @@ class _SmsScreenState extends State<SmsScreen> {
   SmsQuery query;
   ScrollController scrollController;
   ValueNotifier<bool> isExtendedFloatActionBar = ValueNotifier<bool>(true);
+  DateTime currentBackPressTime;
+  final _scaffoldKey = new GlobalKey<ScaffoldState>();
 
   @override
   void initState() {
@@ -33,64 +36,88 @@ class _SmsScreenState extends State<SmsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return NotificationListener<ScrollNotification>(
-      onNotification: (scroll) {
-        isExtendedFloatActionBar.value = scroll.metrics.pixels <= 30;
-        return true;
-      },
-      child: Scaffold(
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          centerTitle: true,
-          title: Text(
-            "Messages",
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.w400,
-            ),
-          ),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.search,
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: NotificationListener<ScrollNotification>(
+        onNotification: (scroll) {
+          isExtendedFloatActionBar.value = scroll.metrics.pixels <= 30;
+          return true;
+        },
+        child: Scaffold(
+          key: _scaffoldKey,
+          extendBodyBehindAppBar: true,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            centerTitle: true,
+            title: Text(
+              "Messages",
+              style: TextStyle(
                 color: Colors.black,
+                fontWeight: FontWeight.w400,
               ),
-              onPressed: () {},
-              tooltip: "Search",
             ),
-            SmsPopupMenuButton()
-          ],
-        ),
-        body: SafeArea(
-          bottom: true,
-          child: RefreshIndicator(
-            onRefresh: () {
-              return SmsBloc.instance.loadSmsthreads();
-            },
-            child: Scrollbar(
-              child: StreamBuilder<List<SmsThread>>(
-                stream: SmsBloc.instance.stream,
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return Center(
-                      child: CircularProgressIndicator(),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(
+                  Icons.search,
+                  color: Colors.black,
+                ),
+                onPressed: () {},
+                tooltip: "Search",
+              ),
+              SmsPopupMenuButton()
+            ],
+          ),
+          body: SafeArea(
+            bottom: true,
+            child: RefreshIndicator(
+              onRefresh: () {
+                return SmsBloc.instance.loadSmsthreads();
+              },
+              child: Scrollbar(
+                child: StreamBuilder<List<SmsThread>>(
+                  stream: SmsBloc.instance.stream,
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                    return BuildSmsMessagesList(
+                      listSmsThread: snapshot.data,
+                      scrollController: scrollController,
                     );
-                  }
-                  return BuildSmsMessagesList(
-                    listSmsThread: snapshot.data,
-                    scrollController: scrollController,
-                  );
-                },
+                  },
+                ),
               ),
             ),
           ),
-        ),
-        floatingActionButton: SmsFloatActionButton(
-          isExtendedFloatActionBar: isExtendedFloatActionBar,
+          floatingActionButton: SmsFloatActionButton(
+            isExtendedFloatActionBar: isExtendedFloatActionBar,
+          ),
         ),
       ),
     );
+  }
+
+  Future<bool> _onWillPop() async {
+    DateTime now = DateTime.now();
+    if (currentBackPressTime == null ||
+        now.difference(currentBackPressTime) > Duration(seconds: 3)) {
+      currentBackPressTime = now;
+      _scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+          content: Text("Press back again to exit."),
+          duration: Duration(seconds: 3),
+          action: SnackBarAction(
+            label: "Cancel",
+            onPressed: () => currentBackPressTime = null,
+          ),
+        ),
+      );
+      return Future.value(false);
+    }
+    return Future.value(true);
   }
 }
